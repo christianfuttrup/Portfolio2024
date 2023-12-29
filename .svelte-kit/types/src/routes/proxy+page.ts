af0a496c23984936153441d1c56ff0127c587e54
Christian Futtrup <cfuttrup.dk@gmail.com>
2023-12-29 23:48:20 +0100

    Update version_hash and add commitData to props

--- .svelte-kit/types/src/routes/proxy+page.ts
+++ .svelte-kit/types/src/routes/proxy+page.ts
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import type { GitHubEvent } from '$lib/types/github';
+import type { GitHubCommit, GitHubData } from '$lib/types/github';
 import type { SpotifyData } from '$lib/types/spotify';
 import type { PageLoad } from './$types';
 
@@ -9,7 +9,8 @@ export const load = (async ({ fetch }) => {
 	const githubResponse = await fetch('https://api.github.com/users/christianfuttrup/events/public');
 
 	let spotifyData: SpotifyData = {} as SpotifyData;
-	let githubData: GitHubEvent[] | { error: string } = [];
+	let githubData: GitHubData[] | { error: string } = [];
+	let commitData: GitHubCommit = {} as GitHubCommit;
 
 	if (!spotifyResponse.ok) {
 		console.error('Failed to fetch data from Spotify:', await spotifyResponse.text());
@@ -24,6 +25,21 @@ export const load = (async ({ fetch }) => {
 	spotifyData = await spotifyResponse.json();
 	githubData = await githubResponse.json();
 
-	return { props: { spotifyData, githubData } };
+	const latestCommitResponse = await fetch(githubData[0].payload.commits[0].url);
+
+	commitData = await latestCommitResponse.json();
+
+	return {
+		props: {
+			spotifyData,
+			githubData,
+			commitData: {
+				sha: githubData[0].payload.commits[0].sha,
+				commit: commitData.commit,
+				committer: commitData.committer,
+				repository: githubData[0].repo
+			}
+		}
+	};
 }) satisfies PageLoad;
 ;null as any as PageLoad;
