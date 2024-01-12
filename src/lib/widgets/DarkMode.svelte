40fcbd77ff2bb9f02c267b3d36950c50b13f58a2
Christian Futtrup <cfuttrup.dk@gmail.com>
2024-01-12 20:09:40 +0100

    Add DarkMode widget

---
+++ src/lib/widgets/DarkMode.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+</script>
+
+<div class="relative flex h-full w-full items-center justify-center border border-black">
+	<div class="relative flex h-[42px] w-20 cursor-pointer flex-col rounded-2xl border bg-sky-300">
+		<div class="absolute left-[5px] top-[5px] h-[30px] w-[30px] rounded-full bg-white"></div>
+	</div>
+</div>
