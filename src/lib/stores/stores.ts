953417e0ef22eb7f865a0df89ebec8734008d133
Christian Futtrup <cfuttrup.dk@gmail.com>
2024-01-13 21:03:18 +0100

    Fix activeItem initialization in stores.ts

--- src/lib/stores/stores.ts
+++ src/lib/stores/stores.ts
@@ -1,4 +1,4 @@
 import { type Writable, writable } from 'svelte/store';
 
-export const activeItem: Writable<string> = writable('All');
+export const activeItem: Writable<string> = writable('All') ;
 export const menuItemWidth: Writable<number> = writable(0);
